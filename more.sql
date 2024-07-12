40fd9943b240ca579cfd42a8a19b3d18e11a6c13
Lewaro <118760347+guesswho-jr@users.noreply.github.com>
2024-07-12 14:39:56 +0300

    Delete more.sql

--- more.sql
+++
@@ -1,7 +0,0 @@
-create table mailing_list (
-    `username` TEXT UNIQUE,
-    code int(6) not NULL,
-    issue_time datetime default current_timestamp,
-    code_sent BOOLEAN DEFAULT false,
-    trials INT DEFAULT 0
-);
